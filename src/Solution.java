74cf7d5c1737583b77309492677390f6aa494c5d
DaniilShelofast <shelofast.daniil@gmail.com>
2023-10-28 00:59:12 +0300

    home task

--- src/Solution.java
+++ src/Solution.java
@@ -2,8 +2,6 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Solution {
-//SELECT NAME FROM product WHERE PRICE >= 100;
-//SELECT NAME FROM product WHERE LENGTH(DESCRIPTION) > 50;
 
     static String userName = "root";
     static String password = "Password";
@@ -15,31 +13,46 @@ public class Solution {
         try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password); Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("select * from Product");
             while (resultSet.next()) {
-                 long productID = resultSet.getLong("ID");
-                 String productName = resultSet.getString("NAME");
-                 String productDescription = resultSet.getString("DESCRIPTION");
-                 int productPrice = resultSet.getInt("PRICE");
-                 Product product = new Product(productID,productName,productDescription,productPrice);
-                 products.add(product);
+                long productID = resultSet.getLong("ID");
+                String productName = resultSet.getString("NAME");
+                String productDescription = resultSet.getString("DESCRIPTION");
+                int productPrice = resultSet.getInt("PRICE");
+                Product product = new Product(productID, productName, productDescription, productPrice);
+                products.add(product);
             }
         }
         return products;
     }
 
-
-
-
-
-
-
-
-    public static void main(String[] args) throws Exception {
-        String userName = "root";
-        String password = "Password";
-        String connectionUrl = "jdbc:mysql://localhost:3306/test";
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static ArrayList<Product> getProductsByPrice() throws SQLException {
+        ArrayList<Product> products = new ArrayList<>();
         try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password); Statement statement = connection.createStatement()) {
-
+            ResultSet resultSet = statement.executeQuery("SELECT NAME FROM product WHERE PRICE >= 100;");
+            while (resultSet.next()) {
+                long productID = resultSet.getLong("ID");
+                String productName = resultSet.getString("NAME");
+                String productDescription = resultSet.getString("DESCRIPTION");
+                int productPrice = resultSet.getInt("PRICE");
+                Product product = new Product(productID, productName, productDescription, productPrice);
+                products.add(product);
+            }
         }
+        return products;
+    }
+
+    public static ArrayList<Product> getProductsByDescription() throws SQLException {
+        ArrayList<Product> products = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT NAME FROM product WHERE LENGTH(DESCRIPTION) > 50;");
+            while (resultSet.next()) {
+                long productID = resultSet.getLong("ID");
+                String productName = resultSet.getString("NAME");
+                String productDescription = resultSet.getString("DESCRIPTION");
+                int productPrice = resultSet.getInt("PRICE");
+                Product product = new Product(productID, productName, productDescription, productPrice);
+                products.add(product);
+            }
+        }
+        return products;
     }
 }
